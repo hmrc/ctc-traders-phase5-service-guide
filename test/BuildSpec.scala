4f7b37a137d1f218d9ecf4402ee9e107366c4b99
Daniel Naylor <93977310+drnaylor-oc@users.noreply.github.com>
2022-04-08 12:56:03 +0100

    CTCP-213: Bring in line with CTC GBQ service guide

--- test/BuildSpec.scala
+++ test/BuildSpec.scala
@@ -16,13 +16,14 @@
 
 package test
 
-import org.scalatest.{Matchers, WordSpec}
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
 import sys.process._
 
-class BuildSpec extends WordSpec with Matchers {
+class BuildSpec extends AnyWordSpec with Matchers {
   "Building the content" should {
     "produce static files" in {
-      val result = "bundle install" #&& Process("bundle exec middleman build --build-dir=public/ --clean", None) !
+      val result = "bundle install" #&& Process("bundle exec middleman build --verbose --build-dir=public/ --clean", None, "BASE_PATH" -> "/guides/ctc-traders-phase5-service-guide/") !
 
       result shouldBe 0
     }
