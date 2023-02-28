e1d0f76aec4aea65499d50a26aedf9993f46a661
fmcgrath <fergal.mcgrath@digital.hmrc.gov.uk>
2023-02-28 16:51:42 +0000

    Add test dependencies to AppDependencies.scala
    Edit BuildSpec.scala
    Edit application-json-logger.xml
    
    Bump up Platform dependencies based on Bot feedback

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,7 +9,8 @@ object AppDependencies {
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % bootStrapPlayVersion)
 
   val test: Seq[ModuleID] = Seq(
-    "org.scalatest" %% "scalatest" % "3.2.9" % "test",
-    "com.vladsch.flexmark" % "flexmark-all" % "0.36.8" % "test"
+    "org.scalatest"       %% "scalatest"              % "3.2.11"             % "test",
+    "com.vladsch.flexmark" % "flexmark-all"           % "0.62.2"             % "test",
+    "uk.gov.hmrc"         %% "bootstrap-test-play-28" % bootStrapPlayVersion % "test"
   )
 }
